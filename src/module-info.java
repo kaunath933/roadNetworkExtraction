dbbfe71f4bc1297c93b9e1d614c1036473d025b1
pinkusrg <pinkusrg@gmail.com>
2019-07-07 12:08:20 +0530

    Initial Commit :

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module  javafx{
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    requires java.desktop;
+    requires javafx.swing;
+
+    opens sample;
+}
---
+++ src/sample/PixelsData.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class PixelsData {
+    public int r,g,b;
+    float h;
+    public int rgb;
+    int v;
+    int label;
+    int index;
+    int flag_b;
+    int x;
+    PixelsData(int rgb,int label,int index,int r,int g,int b,float h){
+        this.rgb = rgb;
+        this.label = label;
+        this.index = index;
+        this.flag_b = -1;
+        this.x = -1;
+        this.v = 0;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.h = h;
+    }
+}
